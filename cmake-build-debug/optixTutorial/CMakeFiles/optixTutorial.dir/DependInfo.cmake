2ced33a47c0cbdcba9b6e7323c7f19e2de58344f
lidianetpereira <lidianetpereira26@gmail.com>
2020-04-16 15:34:42 -0300

    OptixTutorial sample working on Clion

---
+++ cmake-build-debug/optixTutorial/CMakeFiles/optixTutorial.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lidiane/CLionProjects/optix/SDK/optixTutorial/optixTutorial.cpp" "/home/lidiane/CLionProjects/optix/SDK/cmake-build-debug/optixTutorial/CMakeFiles/optixTutorial.dir/optixTutorial.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLUT_FOUND"
+  "GLUT_NO_LIB_PRAGMA"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/lidiane/CLionProjects/optix/include"
+  "../sutil"
+  "../"
+  "/home/lidiane/CLionProjects/optix/include/optixu"
+  "../support/mdl-sdk/include"
+  "."
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/lidiane/CLionProjects/optix/SDK/cmake-build-debug/sutil/CMakeFiles/sutil_sdk.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
